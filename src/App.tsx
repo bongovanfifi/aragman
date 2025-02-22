cdef25f1f2bf6fad62e00157252443ce94ab5ab5
gb <giovanni.b.boff@gmail.com>
2025-02-21 17:01:07 -0800

    center it

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ import { Chip, Input, Button, AccordionItem, Select } from "./components";
 import { getWordMaps } from "./lib/word-maps";
 import { getAnagramKeys } from "./lib/anagrams";
 
+const pageWrapperStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "flex-start",
+  minHeight: "100vh",
+  padding: "20px",
+};
+
 const containerStyle = {
   display: "flex",
   flexDirection: "column" as const,
@@ -208,74 +216,75 @@ function App() {
   }, [remainingLetters, selectedWordMap]);
 
   return (
-    <div style={containerStyle}>
-      {/* TODO: add dropdown to select word list. also, add a smaller wordlist */}
-      <div>
-        <h1>a rag man</h1>
-        <AccordionItem title="How To / About">
-          Type something in to see candidate anagrams of different lengths. You
-          can also add a custom word if the one you want isn't present. There
-          are many word lists to choose from right now, as I find which ones are
-          not bringing anything of value, I'll remove them.
-          <br />
-          The 10k, 20k, and 30k word lists seem possibly lower quality, I think
-          they were collected with scraping? Source{" "}
-          <a href="https://github.com/arstgit/high-frequency-vocabulary">
-            here.
-          </a>{" "}
-          Enable1 and Popular are{" "}
-          <a href="https://github.com/dolph/dictionary/tree/master">here.</a>{" "}
-          Finally, the "Scrabble" list comes from SOWPODS which is...
-          approximately the Scrabble word list? I'm really not sure. But it's
-          not copyrighted, unlike the official list. Source{" "}
-          <a href="https://www.freescrabbledictionary.com/twl06/download/twl06.txt">
-            here.
-          </a>{" "}
-          The lists include information about "keys" rather than "words" because
-          it searches for keys that correspond to sets of words rather than the
-          words themselves.
-          <br />
-          Also, this does searches client side. If you put in a big enough
-          string with a big enough word list, you will crash your browser. I
-          could have prevented this, but I didn't because it is your God-given
-          right to crash your own hardware.
-        </AccordionItem>
-      </div>
-      <div>
-        Word List:{" "}
-        <Select
-          options={options}
-          value={selectedWordMap}
-          onChange={(e) => setSelectedWordMap(e.target.value)}
-        />
-      </div>
-      <div style={inputGroupStyle}>
-        <Input
-          type="text"
-          placeholder="Enter word to anagram"
-          value={baseWord}
-          onChange={(e) => setBaseWord(e.target.value)}
-          style={inputStyle}
-        />
-        {/* <Button
+    <div style={pageWrapperStyle}>
+      <div style={containerStyle}>
+        {/* TODO: add dropdown to select word list. also, add a smaller wordlist */}
+        <div>
+          <h1>a rag man</h1>
+          <AccordionItem title="How To / About">
+            Type something in to see candidate anagrams of different lengths.
+            You can also add a custom word if the one you want isn't present.
+            There are many word lists to choose from right now, as I find which
+            ones are not bringing anything of value, I'll remove them.
+            <br />
+            The 10k, 20k, and 30k word lists seem possibly lower quality, I
+            think they were collected with scraping? Source{" "}
+            <a href="https://github.com/arstgit/high-frequency-vocabulary">
+              here.
+            </a>{" "}
+            Enable1 and Popular are{" "}
+            <a href="https://github.com/dolph/dictionary/tree/master">here.</a>{" "}
+            Finally, the "Scrabble" list comes from SOWPODS which is...
+            approximately the Scrabble word list? I'm really not sure. But it's
+            not copyrighted, unlike the official list. Source{" "}
+            <a href="https://www.freescrabbledictionary.com/twl06/download/twl06.txt">
+              here.
+            </a>{" "}
+            The lists include information about "keys" rather than "words"
+            because it searches for keys that correspond to sets of words rather
+            than the words themselves.
+            <br />
+            Also, this does searches client side. If you put in a big enough
+            string with a big enough word list, you will crash your browser. I
+            could have prevented this, but I didn't because it is your God-given
+            right to crash your own hardware.
+          </AccordionItem>
+        </div>
+        <div>
+          Word List:{" "}
+          <Select
+            options={options}
+            value={selectedWordMap}
+            onChange={(e) => setSelectedWordMap(e.target.value)}
+          />
+        </div>
+        <div style={inputGroupStyle}>
+          <Input
+            type="text"
+            placeholder="Enter word to anagram"
+            value={baseWord}
+            onChange={(e) => setBaseWord(e.target.value)}
+            style={inputStyle}
+          />
+          {/* <Button
           text="Search for anagrams"
           onClick={() => {
             setSelectedWords([]);
             handleSearch(cleanInput(baseWord));
           }}
         /> */}
-      </div>
-      <div style={inputGroupStyle}>
-        <Input
-          type="text"
-          placeholder="Enter custom word"
-          value={subWord}
-          onChange={(e) => setSubWord(e.target.value)}
-          style={inputStyle}
-        />
-        <Button text="Add word" onClick={() => handleSelectWord(subWord)} />
-      </div>
-      {/* <div style={inputGroupStyle}>
+        </div>
+        <div style={inputGroupStyle}>
+          <Input
+            type="text"
+            placeholder="Enter custom word"
+            value={subWord}
+            onChange={(e) => setSubWord(e.target.value)}
+            style={inputStyle}
+          />
+          <Button text="Add word" onClick={() => handleSelectWord(subWord)} />
+        </div>
+        {/* <div style={inputGroupStyle}>
         <Button
           text="Search using remaining letters"
           onClick={() => handleSearch(remainingLetters.join(""))}
@@ -286,38 +295,39 @@ function App() {
           // TODO: disabled needs to have a visual indicator! add an option to disable auto search, then enable this, also
         />
       </div> */}
-      {usedLetters.length > 0 && (
-        <div>
-          <div style={sectionStyle}>Letters Used</div>
-          {usedLetters.join(" ")}
-        </div>
-      )}
-      {remainingLetters.length > 0 &&
-        selectedWords.length > 0 &&
-        remainingLetters.length < cleanInput(baseWord).length && (
+        {usedLetters.length > 0 && (
           <div>
-            <div style={sectionStyle}>Letters Remaining</div>
-            {remainingLetters.join(" ")}
+            <div style={sectionStyle}>Letters Used</div>
+            {usedLetters.join(" ")}
           </div>
         )}
-      {excessLetters.length > 0 && (
-        <div>
-          <div style={sectionStyle}>Excess Letters!</div>
-          {excessLetters.join(" ")}
-        </div>
-      )}
-      {selectedWords.length > 0 && (
-        <div>
-          <div style={sectionStyle}>Selected Words:</div>
-          {makeSelected(selectedWords)}
-        </div>
-      )}
-      {candidates.size > 0 && (
-        <div>
-          <div style={sectionStyle}>Candidates:</div>
-          {Candidates(candidates)}
-        </div>
-      )}
+        {remainingLetters.length > 0 &&
+          selectedWords.length > 0 &&
+          remainingLetters.length < cleanInput(baseWord).length && (
+            <div>
+              <div style={sectionStyle}>Letters Remaining</div>
+              {remainingLetters.join(" ")}
+            </div>
+          )}
+        {excessLetters.length > 0 && (
+          <div>
+            <div style={sectionStyle}>Excess Letters!</div>
+            {excessLetters.join(" ")}
+          </div>
+        )}
+        {selectedWords.length > 0 && (
+          <div>
+            <div style={sectionStyle}>Selected Words:</div>
+            {makeSelected(selectedWords)}
+          </div>
+        )}
+        {candidates.size > 0 && (
+          <div>
+            <div style={sectionStyle}>Candidates:</div>
+            {Candidates(candidates)}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
